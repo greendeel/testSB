7799755760366d1f914657a76e84c5858c62fa65
greendeel <greendeel@gmail.com>
2026-02-06 22:33:48 +0100

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -38,7 +38,7 @@ const Navigation: React.FC<NavigationProps> = ({
       <div className="flex items-center justify-between px-4 py-1.5 border-b border-slate-800">
         <button 
           onClick={onExit}
-          className="self-center flex items-center justify-center gap-2 bg-slate-800 hover:bg-slate-700 px-3 h-9 rounded-lg border border-slate-600 font-black transition-all active:scale-95 text-yellow-400 group cursor-pointer"
+          className="self-center flex items-center justify-center gap-1 bg-slate-800 hover:bg-slate-700 px-3 h-4 rounded-lg border border-slate-600 font-black transition-all active:scale-95 text-yellow-400 group cursor-pointer"
         >
           <ChevronLeft size={16} />
           <span className="text-base uppercase tracking-wider">Menu</span>
